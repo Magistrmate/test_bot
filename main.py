7c3dae4e645023f438324b60925c0744d5249309
Magistrmate <101048039+Magistrmate@users.noreply.github.com>
2023-09-29 09:08:22 +0000

    process in database

--- main.py
+++ main.py
@@ -18,19 +18,11 @@ default_app = firebase_admin.initialize_app(  # type: ignore
 bot = telebot.TeleBot(os.environ['TOKEN'])
 chats_with_bot_id = int(os.environ['CHATS_WITH_BOT_ID'])
 
-
-def send(id_user, text, id_topic, markup):
-  bot.send_message(chats_with_bot_id, text, message_thread_id=id_topic)
-  if id_user != 0:
-    bot.send_message(id_user, text, reply_markup=markup)
-
-
-@bot.message_handler(func=lambda _message: True, chat_types=['private'])
-def send_message(message):
-  id_user = message.from_user.id
-  first_name = message.from_user.first_name
-  last_name = message.from_user.last_name
+def id_topic_target(m):
+  id_user = m.from_user.id
   if db.reference(f'/users/{id_user}').get() is None:
+    first_name = m.from_user.first_name
+    last_name = m.from_user.last_name
     topic = bot.create_forum_topic(
         chats_with_bot_id, f'{first_name} {last_name}',
         random.choice(
@@ -39,29 +31,41 @@ def send_message(message):
     db.reference(f'/users/{id_user}/id_topic').set(id_topic)
   else:
     id_topic = db.reference(f'/users/{id_user}/id_topic').get(etag=True)[0]
+  return id_topic
 
-  send(0, f'@{message.from_user.username}\n{message.text}', id_topic, 0)
-  db.reference(f'/users/{id_user}/{message.id}').set(message.json)
+def send(m, text, markup):
+  id_user = m.from_user.id
+  id_topic = id_topic_target(m)
+  bot.send_message(chats_with_bot_id, text, message_thread_id=id_topic)
+  bot.send_message(id_user, text, reply_markup=markup)
+  db.reference(f'/users/{id_user}/{m.id}').set(m.json)
+  db.reference(f'/users/{id_user}/{m.id}/answer_bot').set(text)
+
+
+@bot.message_handler(func=lambda _message: True, chat_types=['private'])
+def send_message(message):
+  id_user = message.from_user.id
+  send(message, f'@{message.from_user.username}\n{message.text}', 0)
   if db.reference(f'/users/{id_user}/link_channel').get() is None:
-    send(id_user,
-         db.reference('/script/start_text').get(), id_topic,
-         types.ForceReply(input_field_placeholder='Ссылка на канал'))
+    send(message,
+         db.reference('/script/start_text').get(),
+         types.ForceReply(True, 'Ссылка на канал'))
     db.reference(f'/users/{id_user}/link_channel').set('wait')
   elif db.reference(f'/users/{id_user}/link_channel').get() == 'wait':
     if message.entities is not None:
       if message.entities[0].type == 'url':
-        send(id_user, 'красава', id_topic,
-             types.ForceReply(input_field_placeholder='Ссылка на канал'))
+        send(message, 'Отлично!', 0)
         offset = message.entities[0].offset
         length = message.entities[0].length
-        db.reference(f'/users/{id_user}/link_channel').set(\
+        db.reference(f'/users/{id_user}/link_channel').set(
           message.text[offset:offset + length])
       else:
-        send(id_user, 'ссылку оло', id_topic,
-             types.ForceReply(input_field_placeholder='Ссылка на канал'))
+        send(message, 'Не вижу ссылки на канал в вашем сообщении',
+             types.ForceReply(True, 'Ссылка на канал'))
     else:
-      send(id_user, 'гони быстр', id_topic,
-           types.ForceReply(input_field_placeholder='Ссылка на канал'))
+      send(message, 'Ответ должен содержать ссылку на канал',
+           types.ForceReply(True, 'Ссылка на канал'))
+  
 
 
 bot.infinity_polling()
