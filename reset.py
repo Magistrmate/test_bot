14cc011724f26874306422f30fe8d62c9373a5f7
Magistrmate <101048039+Magistrmate@users.noreply.github.com>
2024-02-26 13:54:22 +0000

    Here we go

--- reset.py
+++ reset.py
@@ -1,17 +1,8 @@
-import json
-import os
-
-import firebase_admin
-from firebase_admin import credentials  # type: ignore
-
-cred = credentials.Certificate(json.loads(os.environ['KEY']))
-
-app = firebase_admin.initialize_app(  # type: ignore
-    cred, {
-        'databaseURL':
-        'https://big-signifier-398111-default-rtdb.firebaseio.com/'
-    })
-
-text = "Класс! Вы зарегистрированы. Теперь посмотрим кому нужно помочь в первую очередь. Если всё же хотите ознакомиться с другими каналами, то нажмите на кнопки ниже."
-text = text.replace('_', '\\_').replace('*', '\\*').replace('[', '\\[').replace(']', '\\]').replace('(', '\\(').replace(')', '\\)').replace('~', '\\~').replace('"', '\"').replace('>', '\\>').replace('#', '\\#').replace('+', '\\+').replace('-', '\\-').replace('=', '\\=').replace('|', '\\|').replace('{', '\\{').replace('}','\\}').replace('.', '\\.').replace('!', '\\!')
-print(text)
+x = 0
+y = 1
+if x == 4:
+ y = 1
+elif x == 3:
+ y = 3
+else:
+ y = 2
